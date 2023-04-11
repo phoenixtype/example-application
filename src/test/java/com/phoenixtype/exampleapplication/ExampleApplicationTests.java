2ed7946593c27a0f3dc862a35679c2d4b735706d
Samuel Akuma <usakuma@uwaterloo.ca>
2023-04-10 21:51:10 -0400

    Initial commit

---
+++ src/test/java/com/phoenixtype/exampleapplication/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixtype.exampleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
